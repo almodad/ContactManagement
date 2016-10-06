ed949241da337f375c7b78f894b55de5df9edab4
RAMEA SOLUTIONS <rameasolutions@gmail.com>
2016-10-06 15:41:30 +0300

    added proxy class

---
+++ ContactManagement/Proxy.cs
@@ -0,0 +1,35 @@
+﻿using PortableRest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContactManagement
+{
+    public class Proxy
+    {
+        private const string URL = "http://apicontacts.azurewebsites.net/";
+        public static Contact postContact(Contact contact)
+        {
+            var client = new RestClient { BaseUrl = URL };
+
+            RestRequest request = new RestRequest("contact/", HttpMethod.Post);
+            request.AddParameter(contact);
+            var response = client.ExecuteAsync<Contact>(request);
+            
+            return response.Result;          
+        }
+
+        public static List<Contact> getContact()
+        {
+            var client = new RestClient { BaseUrl = URL };
+            RestRequest request = new RestRequest("contact/", HttpMethod.Get);
+            var response = client.ExecuteAsync<List<Contact>>(request);
+            return response.Result;
+        }
+
+
+    }
+}
